e033e51914527a60ce56eec4247b050523235efe
Nidhi Mylavarapu <nm549@cornell.edu>
2023-08-07 10:15:35 -0400

    starting new auth + authcontext

--- src/app.tsx
+++ src/app.tsx
@@ -2,23 +2,23 @@ import { AppProps } from "next/app"
 import {
     BrowserRouter,
     Routes,
-    Route
+    Route,
+    Navigate
   } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/home";
 import Register from "./pages/register";
 import Login from "./pages/login";
-import { AuthContext, AuthContextProvider } from "./context/AuthContext";
 import { useContext } from "react";
 
 function App() {
-  const {currentUser} = useContext(AuthContext)
-  console.log(currentUser)
+
+  
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<Home />}/>
+          <Route index element=<Home /> />
           <Route path="register" element={<Register />}/>
           <Route path="login" element={<Login />}/>
         </Route>
